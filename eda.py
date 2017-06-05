d57367005757e70f93d9f699243d11116f526e0f
CausticGiraffe <karlie.ekstrom@gmail.com>
2017-06-04 20:46:16 -0600

    adding eda

---
+++ eda.py
@@ -0,0 +1,12 @@
+import numpy as np
+import skimage.io as io
+import keras
+import os
+import glob
+import matplotlib.pyplot as plt
+
+
+#datapath = os.getcwd()+'/data/train'
+
+#datalist = os.listdir(datapath)
+#len(datalist)
